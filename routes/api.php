f5299fe6d2db67ec7765adca0744cf09901bab80
IBorge <isaacbg2003@gmail.com>
2024-02-19 19:11:36 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostXuxe;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', [PostXuxe::class, 'index'])->name('index');
+Route::get('/show/{id}', [PostXuxe::class, 'show'])->name('show');
+Route::post('/store', [PostXuxe::class, 'store'])->name('store');
+Route::post('/update/{id}', [PostXuxe::class, 'update'])->name('update');
+Route::delete('/delete/{id}', [PostXuxe::class, 'destroy'])->name('destroy');
