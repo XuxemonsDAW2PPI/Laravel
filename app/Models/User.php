4a44e0a809e0c4172bd0163ec13f0c7479469237
IBorge <isaacbg2003@gmail.com>
2024-02-23 20:10:55 +0100

    b

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,36 +10,14 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    use HasFactory;
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'id',
+        'Nombre',
+        'Contraseña',
+        'Correo',
+        'UserType',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('Nombre');
+            $table->string('Correo')->unique();
+            $table->string('Contraseña');
+            $table->string('UserType');
         });
     }
 
@@ -29,4 +27,7 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
+
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
 };
