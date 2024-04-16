fa18adf52fb04b91a7963cb49c2c61eac34ca0a0
pierosanz2 <128500925+pierosanchez098@users.noreply.github.com>
2024-04-16 19:01:58 +0200

    Para que se haga delete correctamente a un user desde mysql

--- database/migrations/2024_03_06_175429_inv_xuxe_user.php
+++ database/migrations/2024_03_06_175429_inv_xuxe_user.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('imagen');
             $table->string('estado');
 
-            $table->foreign('idusuario')->references('id')->on('users');
+            $table->foreign('idusuario')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('idxuxemon')->references('id')->on('xuxemons');
         });
     }
