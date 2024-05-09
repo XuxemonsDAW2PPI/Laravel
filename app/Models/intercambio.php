a19de3828ca120c6ce6214f3313555ddc5453b8f
pierosanz2 <128500925+pierosanchez098@users.noreply.github.com>
2024-05-09 20:17:43 +0200

    Update intercambiar xuxemon

---
+++ app/Models/intercambio.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class intercambio extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'idusuario1',
+        'usertag1',
+        'nombre_xuxemon1',
+        'tamano_xuxemon1',
+        'caramelos_comidosx1',
+        'idusuario2',
+        'usertag2',
+        'nombre_xuxemon2',
+        'tamano_xuxemon2',
+        'caramelos_comidosx2',
+        'estado'
+    ];
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+}
---
+++ database/migrations/2024_05_09_170453_solicitudes_intercambio.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('intercambios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idusuario1');
+            $table->string('usertag1');
+            $table->string('nombre_xuxemon1');
+            $table->string('tamano_xuxemon1');
+            $table->unsignedBigInteger('caramelos_comidosx1');
+            $table->unsignedBigInteger('idusuario2');
+            $table->string('usertag2');
+            $table->string('nombre_xuxemon2');
+            $table->string('tamano_xuxemon2');
+            $table->unsignedBigInteger('caramelos_comidosx2');
+            $table->string('estado');
+
+            $table->foreign('idusuario1')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('idusuario2')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
